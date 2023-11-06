49ae599dfe64288df7d3e21df64d496342d8b504
MinhTris <boyds_24854@yahoo.com.vn>
2023-11-06 10:32:11 +0700

    Update first challenge

--- app/src/main/java/com/example/basiccourse/MainActivity.java
+++ app/src/main/java/com/example/basiccourse/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.basiccourse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /*
+    public void button (View view){
+    TextView enterFirst = findViewById(R.id.txtFirst);
+    TextView enterSurface = findViewById(R.id.txtSurface);
+    TextView enterEmail = findViewById(R.id.txtEmail);
+
+    EditText getname = findViewById(R.id.edtFirst);
+    EditText getsurface = findViewById(R.id.edtSurface);
+    EditText getemail= findViewById(R.id.edtEmail);
+
+    enterFirst.setText("Your first name " + getname.getText().toString());
+    enterSurface.setText("Your surface name " + getsurface.getText().toString());
+    enterEmail.setText("Your email " + getemail.getText().toString());
+    }
+
+     */
+
 }
